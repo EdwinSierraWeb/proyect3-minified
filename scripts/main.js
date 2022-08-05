8d148c153d9b7b4ff88e232a372b873374c966d1
Edwin Daniel Landeros Sierra <107223067+EdwinSierraWeb@users.noreply.github.com>
2022-08-04 22:11:26 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+const titles=document.querySelectorAll('.titles');const observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{entry.target.classList.toggle('titles11',entry.isIntersecting)})},{rootMargin:"0px 0px 50px 0px",threshold:0,})
+titles.forEach(title=>{observer.observe(title)})
